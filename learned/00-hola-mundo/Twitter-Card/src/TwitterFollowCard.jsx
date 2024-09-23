965991f05c2df580818c860194c46bed658eceaf
Nekane <125030234+nmontiel42@users.noreply.github.com>
2024-09-23 11:07:16 +0200

    Update TwitterFollowCard.jsx

--- learned/00-hola-mundo/Twitter-Card/src/TwitterFollowCard.jsx
+++ learned/00-hola-mundo/Twitter-Card/src/TwitterFollowCard.jsx
@@ -30,7 +30,8 @@ export function TwitterFollowCard ({ children, userName }) {
 
             <aside>
                 <button className={buttonClassName} onClick={handleClick}>
-                    {text} 
+                    <span className='tw-followCard-text'>{text}</span> 
+                    <span className='tw-followCard-stopFollow'>Dejar de seguir</span>
                 </button>
             </aside>
         </article>
