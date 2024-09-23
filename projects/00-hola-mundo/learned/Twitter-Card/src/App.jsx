78a0e9cabad49b2fe5899125b18b8d0684d37942
nmontiel42 <montielarce9@gmail.com>
2024-09-23 11:21:39 +0200

    learned added

---
+++ projects/00-hola-mundo/learned/Twitter-Card/src/App.jsx
@@ -0,0 +1,37 @@
+import './App.css'
+import { TwitterFollowCard } from './TwitterFollowCard'
+
+export function App () {
+    return (
+        <section className="App">
+            <TwitterFollowCard userName="nmontiel42"> 
+                Nekane Montiel 
+            </TwitterFollowCard> 
+
+            <TwitterFollowCard userName="abello-r">
+                Alexander Bello Rodriguez
+             </TwitterFollowCard>
+
+            <TwitterFollowCard userName="pausanpi">
+                Paula Sanchez Piña
+            </TwitterFollowCard>
+
+            {/*Los comentarios con /* y las llaves {} dentro del render */}
+        </section>
+    )
+}
+
+/*Tambien se puede pasar {true} poniendo solo el booleano (isFollowing)
+isFollowing={true} = isFollowing */
+
+/*Podriamos usar children en vez de name, ya que el nombre estaria contenido dentro
+de TwitterFollowCard, por lo que seria su hijo. Children no es un prop normal, es una
+mala practica usarlo como tal */
+
+/* Los nombres serian los children */
+
+/*Se pueden pasar props juntas creando un const con la info y pasando 
+{... nombreConst} al componente */
+
+/* Podemos pasar un prop de valor inicial para inicializar un estado. Por ejemplo
+podriamos pasar initialIsFollowing para indicar el estado inicial */
---
+++ projects/00-hola-mundo/learned/Twitter-Card/src/TwitterFollowCard.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react"
+
+//Sirve para poder guardar una variable
+
+export function TwitterFollowCard ({ children, userName }) {
+
+    const [isFollowing, setIsFollowing] = useState(false)
+    
+    /*  ==
+    const state = useState(false)
+    const isFollowing = state[0]
+    const setIsFollowing = state[1] */
+
+    const text = isFollowing ? 'Siguiendo' : 'Seguir'
+    const buttonClassName = isFollowing ? 'tw-followCard-button is-following' : 'tw-followCard-button'
+    
+    const handleClick = () => {
+        setIsFollowing(!isFollowing) //Cambia el estado de esa variable
+    }
+
+    return (
+        <article className='tw-followCard'>
+            <header className='tw-followCard-header'>
+                <img className='tw-followCard-img' alt="Mi avatar" src={`https://unavatar.io/${userName}`} />
+                <div>
+                    <strong className='tw-followCard-strong'>{children}</strong>
+                    <span className='tw-followCard-span'>@{userName}</span>
+                </div>
+            </header>
+
+            <aside>
+                <button className={buttonClassName} onClick={handleClick}>
+                    {text} 
+                </button>
+            </aside>
+        </article>
+    )
+}
